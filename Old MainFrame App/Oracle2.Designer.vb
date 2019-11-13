3d97434ff8ce39d412f48a26430a04fb555b93ab
Andrew Coates <acoat@microsoft.com>
2019-11-14 10:06:46 +1100

    Add new form and make it default

---
+++ Old MainFrame App/Oracle2.Designer.vb
@@ -0,0 +1,311 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Oracle2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress1 = New System.Windows.Forms.TextBox()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtAddress2 = New System.Windows.Forms.TextBox()
+        Me.txtZip = New System.Windows.Forms.TextBox()
+        Me.txtState = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Lime
+        Me.Label1.Location = New System.Drawing.Point(15, 17)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(269, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "YHPR,MWDEMO1 ,"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.ForeColor = System.Drawing.Color.Purple
+        Me.Label2.Location = New System.Drawing.Point(268, 17)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 37)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "P"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Lime
+        Me.Label3.Location = New System.Drawing.Point(295, 17)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(161, 37)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = ",MW00001"
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(295, 102)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(611, 37)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "CUSTOMER NAME/ADDRESS MAINTENANCE"
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(15, 600)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(1133, 37)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "F1=Help F3=Exit F4=Add F5=Updt F6=Delt F7=Back F8=Fwd F10=Part"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(181, 161)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(161, 37)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "CUSTOMER"
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label7.AutoSize = True
+        Me.Label7.ForeColor = System.Drawing.Color.Cyan
+        Me.Label7.Location = New System.Drawing.Point(181, 209)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(89, 37)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "NAME"
+        '
+        'Label8
+        '
+        Me.Label8.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label8.AutoSize = True
+        Me.Label8.ForeColor = System.Drawing.Color.Cyan
+        Me.Label8.Location = New System.Drawing.Point(181, 260)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(161, 37)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "ADDRESS1"
+        '
+        'Label9
+        '
+        Me.Label9.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label9.AutoSize = True
+        Me.Label9.ForeColor = System.Drawing.Color.Cyan
+        Me.Label9.Location = New System.Drawing.Point(181, 311)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(161, 37)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "ADDRESS2"
+        '
+        'Label10
+        '
+        Me.Label10.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label10.AutoSize = True
+        Me.Label10.ForeColor = System.Drawing.Color.Cyan
+        Me.Label10.Location = New System.Drawing.Point(181, 464)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(71, 37)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "ZIP"
+        '
+        'Label11
+        '
+        Me.Label11.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label11.AutoSize = True
+        Me.Label11.ForeColor = System.Drawing.Color.Cyan
+        Me.Label11.Location = New System.Drawing.Point(181, 413)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(107, 37)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "STATE"
+        '
+        'Label12
+        '
+        Me.Label12.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label12.AutoSize = True
+        Me.Label12.ForeColor = System.Drawing.Color.Cyan
+        Me.Label12.Location = New System.Drawing.Point(181, 362)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(89, 37)
+        Me.Label12.TabIndex = 9
+        Me.Label12.Text = "CITY"
+        '
+        'txtName
+        '
+        Me.txtName.BackColor = System.Drawing.Color.Black
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtName.ForeColor = System.Drawing.Color.Lime
+        Me.txtName.Location = New System.Drawing.Point(401, 207)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(505, 45)
+        Me.txtName.TabIndex = 12
+        '
+        'txtAddress1
+        '
+        Me.txtAddress1.BackColor = System.Drawing.Color.Black
+        Me.txtAddress1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtAddress1.ForeColor = System.Drawing.Color.Lime
+        Me.txtAddress1.Location = New System.Drawing.Point(401, 258)
+        Me.txtAddress1.Name = "txtAddress1"
+        Me.txtAddress1.Size = New System.Drawing.Size(505, 45)
+        Me.txtAddress1.TabIndex = 13
+        '
+        'txtCity
+        '
+        Me.txtCity.BackColor = System.Drawing.Color.Black
+        Me.txtCity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtCity.ForeColor = System.Drawing.Color.Lime
+        Me.txtCity.Location = New System.Drawing.Point(401, 360)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(505, 45)
+        Me.txtCity.TabIndex = 15
+        '
+        'txtAddress2
+        '
+        Me.txtAddress2.BackColor = System.Drawing.Color.Black
+        Me.txtAddress2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtAddress2.ForeColor = System.Drawing.Color.Lime
+        Me.txtAddress2.Location = New System.Drawing.Point(401, 309)
+        Me.txtAddress2.Name = "txtAddress2"
+        Me.txtAddress2.Size = New System.Drawing.Size(505, 45)
+        Me.txtAddress2.TabIndex = 14
+        '
+        'txtZip
+        '
+        Me.txtZip.BackColor = System.Drawing.Color.Black
+        Me.txtZip.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtZip.ForeColor = System.Drawing.Color.Lime
+        Me.txtZip.Location = New System.Drawing.Point(401, 462)
+        Me.txtZip.Name = "txtZip"
+        Me.txtZip.Size = New System.Drawing.Size(505, 45)
+        Me.txtZip.TabIndex = 17
+        '
+        'txtState
+        '
+        Me.txtState.BackColor = System.Drawing.Color.Black
+        Me.txtState.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtState.ForeColor = System.Drawing.Color.Lime
+        Me.txtState.Location = New System.Drawing.Point(401, 411)
+        Me.txtState.Name = "txtState"
+        Me.txtState.Size = New System.Drawing.Size(505, 45)
+        Me.txtState.TabIndex = 16
+        '
+        'Oracle2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(18.0!, 37.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(1178, 644)
+        Me.Controls.Add(Me.txtZip)
+        Me.Controls.Add(Me.txtState)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.txtAddress2)
+        Me.Controls.Add(Me.txtAddress1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Consolas", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Oracle2"
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Oracle v1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress1 As TextBox
+    Friend WithEvents txtCity As TextBox
+    Friend WithEvents txtAddress2 As TextBox
+    Friend WithEvents txtZip As TextBox
+    Friend WithEvents txtState As TextBox
+End Class
---
+++ Old MainFrame App/Oracle2.vb
@@ -0,0 +1,41 @@
+﻿Public Class Oracle2
+
+    ' From https://stackoverflow.com/questions/4630825/capture-function-keys-f1-f12-in-vb-net
+    Protected Overrides Function ProcessCmdKey(ByRef msg As System.Windows.Forms.Message, ByVal keyData As System.Windows.Forms.Keys) As Boolean
+        Select Case keyData
+
+            Case Keys.Space
+                FillTextBox()
+
+            Case Keys.F5
+                MessageBox.Show("Record:" & vbCrLf & txtName.Text & vbCrLf & "Updated", "Skynet Systems")
+                Application.Exit()
+
+
+            Case Else
+                Return MyBase.ProcessCmdKey(msg, keyData)
+
+        End Select
+
+        Return True
+    End Function
+
+    Protected Sub FillTextBox()
+        Select Case Me.ActiveControl.Name
+            Case NameOf(txtName)
+                txtName.Text = "Charles ""Scaley"" Sterling"
+            Case NameOf(txtAddress1)
+                txtAddress1.Text = "In a Fishing Boat"
+            Case NameOf(txtAddress2)
+                txtAddress2.Text = "Somewhere off the"
+            Case NameOf(txtCity)
+                txtCity.Text = "GOLD COAST"
+            Case NameOf(txtState)
+                txtState.Text = "QLD"
+            Case NameOf(txtZip)
+                txtZip.Text = "4217"
+        End Select
+        Me.GetNextControl(Me.ActiveControl, True)?.Select()
+        DirectCast(Me.ActiveControl, TextBox).Select(DirectCast(Me.ActiveControl, TextBox).Text.Length + 1, 1)
+    End Sub
+End Class
